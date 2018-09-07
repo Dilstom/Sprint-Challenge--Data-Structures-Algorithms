7e385d1f15e36b9bf0ca4a73e1ee7054be05fe32
Dil Sombayeva <dilsombayeva@KevinsMacBook2.attlocal.net>
2018-09-07 14:22:18 -0700

    after solution video

--- data_structures/ex_1/binary_search_tree.py
+++ data_structures/ex_1/binary_search_tree.py
@@ -5,26 +5,60 @@ class BinarySearchTree:
     self.right = None
 
   def depth_first_for_each(self, cb):
+      # Recursive implementation
+      # call the cb on th current node
     cb(self.value)
+      #check if this node has a left child
     if self.left:
+      #call dfs on the left child
         self.left.depth_first_for_each(cb)
+      #check if this node has a right child 
     if self.right:
+      # call dfs on the right child
         self.right.depth_first_for_each(cb) 
+      
+    # *** V2 *** iterative implementation
+    def depth_first_for_each(self, cb):
+        # initialize a list to be our stack
+        stack = []
+        # add the root node to the dtack
+        stack.append(self)
+        # loop as long as we have elements in the stack
+        while len(stack):
+            # pop off the stack
+            current_node = stack.pop()
+            # check if the popped-off node has a right child
+            if current_node.right:
+            # add the right child to the stack
+                stack.append(current_node.right)
+            # check if the popped-off node has a left child
+            if current_node.left:
+            # add the left child to the stack
+                stack.append(current_node.right)
+            #invoke the cb on the popped-off node
+            cb(current_node.value)
 
-  def breadth_first_for_each(self, cb):
-    arr = []
-    arr.append(self)
-    while len(arr) > 0:
-        node = arr.pop()
-        if node.left:
-            arr.append(node.left)
-        if node.right:
-            arr.append(node.right)
-        cb(node.value)
-        
-
-
-
+    # solution - the most expensive because of array (shift all values)
+    def breadth_first_for_each(self, cb):
+    # initialize a list to be our queue
+    queue = []
+    # add the root node to the queue
+    queue.append(self)
+    # loop as long as we have elements in the queue
+    while len(queue):
+        # pop off the queue
+        current_node = queue.pop(0)
+        # check if the popped-off node has a left child
+        if current_node.left:
+        # add the left child to the queue
+            queue.append(current_node.left)
+        # check if the popped-off node has a right child
+        if current_node.right:
+        # add the right child to the queue
+            queue.append(current_node.right)
+        #invoke the cb on the popped-off node
+        cb(current_node.value)
+     
   def insert(self, value):
     new_tree = BinarySearchTree(value)
     if (value < self.value):
